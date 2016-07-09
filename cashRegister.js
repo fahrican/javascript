8ccf365bf5f7c58c65dd363097698db4c494c1cd
Fahri Can <fahri-can@gmx.at>
2016-07-09 13:50:47 +0200

    this is my own version

--- cashRegister.js
+++ cashRegister.js
@@ -1,48 +1,73 @@
-function StaffMember(name,discountPercent){
-    this.name = name;
-    this.discountPercent = discountPercent;
-}
-
-var sally = new StaffMember("Sally",5);
-var bob = new StaffMember("Bob",10);
-
-// Create yourself again as 'me' with a staff discount of 20%
-var me = new StaffMember("Fahri", 20);
+// JavaScript File
 
 var cashRegister = {
-    total:0,
-    lastTransactionAmount: 0,
+
+    total: 0,
+
+    theLast: 0,
+
     add: function(itemCost){
-        this.total += (itemCost || 0);
-        this.lastTransactionAmount = itemCost;
+
+        this.total += itemCost;
+        this.theLast = itemCost;
     },
-    scan: function(item,quantity){
-        switch (item){
-        case "eggs": this.add(0.98 * quantity); break;
-        case "milk": this.add(1.23 * quantity); break;
-        case "magazine": this.add(4.99 * quantity); break;
-        case "chocolate": this.add(0.45 * quantity); break;
+
+    scan: function(anyItem, quantity){
+
+        switch(anyItem){
+
+            case "magazine":
+                this.add(20.40 * quantity);
+                break;
+
+            case "milk":
+                this.add(1.49 * quantity);
+                break;
+
+            case "beef":
+                this.add(11.99 * quantity);
+                break;
+
+            case "candies":
+                this.add(2.00 * quantity);
+                break;
+
+            default:
+                console.log("Something went wrong!");
+
         }
-        return true;
     },
-    voidLastTransaction : function(){
-        this.total -= this.lastTransactionAmount;
-        this.lastTransactionAmount = 0;
+
+    deleteLast: function(){
+
+        this.total -= this.theLast;
+        this.theLast = 0;
     },
-    // Create a new method applyStaffDiscount here
-    applyStaffDiscount: function(employee){
-    
-        this.total -= this.total * (employee.discountPercent / 100);
+
+    getDiscount: function(employee){
+
+        cashRegister.total -= cashRegister.total * employee.discount / 100;        
     }
-    
 };
 
-cashRegister.scan('eggs',1);
-cashRegister.scan('milk',1);
-cashRegister.scan('magazine',3);
-// Apply your staff discount by passing the 'me' object 
-// to applyStaffDiscount
-cashRegister.applyStaffDiscount(me)
+// here is the discount function for the staff
+function staffDiscount(name, discount){
 
-// Show the total bill
-console.log('Your bill is '+cashRegister.total.toFixed(2));
+    this.name = name;
+    this.discount = discount;
+};
+
+// here I start to run my program
+var itsMe = new staffDiscount("Fahri", 30);
+
+cashRegister.scan("candies", 3);
+console.log(cashRegister.total);
+
+cashRegister.scan("magazine", 2);
+console.log(cashRegister.total);
+
+cashRegister.deleteLast();
+console.log(cashRegister.total);
+
+cashRegister.getDiscount(itsMe);
+console.log(cashRegister.total);
