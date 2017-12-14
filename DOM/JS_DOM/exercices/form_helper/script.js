9784ff1952450dab9038b3f9dc4ce8bbfdcb44aa
Fahri Can <fahri-can@gmx.at>
2017-12-14 23:07:22 +0100

    Update script.js

--- DOM/JS_DOM/exercices/form_helper/script.js
+++ DOM/JS_DOM/exercices/form_helper/script.js
@@ -46,36 +46,56 @@ FormHelper = {
 
 			anArrayElement[i].addEventListener(eventType, element => {
 
-				if (eventType === "focus") {
+			// [dn] zuweisen um zeichenlaenge zu sparen
 
-					if(element.target.name === nameOfField1){
-						element.target.style.backgroundColor = color;
-						//here starts my problem
-						let test = that.descriptiveText(nameOfField1);
-						element.target.appendChild(test);
-					}
-					else if(element.target.name === nameOfField2){
-						element.target.style.backgroundColor = color;
-						//here starts my problem
-						element.target.appendChild(that.descriptiveText(nameOfField2));
-					}	
-				}
-				else if (eventType === "focusout") {
+			var dn = element.target;
 
-					if(element.target.name === nameOfField1){
-						element.target.style.backgroundColor = color;
-						//here starts my problem
-						element.target.removeChild(element.firstChild);
-					}
-					else if(element.target.name === nameOfField2){
-						element.target.style.backgroundColor = color;
-						//here starts my problem
-						element.target.removeChild(element.firstChild);
-					}	
-				}
-				
-			});
+			if (eventType === "focus") {
+
+				dn.style.backgroundColor = color;
+
+				// hier holen wir uns den passenden text
+
+				// entweder parameter drei oder vier
+
+				test = ( dn.name == nameOfField1 )
+
+				? that.descriptiveText(nameOfField1)
+
+				: that.descriptiveText(nameOfField2)
+
+				// jetzt schreiben wir den node _nach_ dem 
+
+				// form element node
+
+				dn.parentNode.insertBefore( test, dn.nextSibling );
+
+			}
+
+			else if (eventType === "focusout") {
+
+				dn.style.backgroundColor = color;
+
+				// hier holen wir uns den description node
+
+				// in diesem Fall der naechste Schwesternode
+
+				// des input nodes
+
+				var dnDes = dn.nextElementSibling;
+
+
+
+				// und zerstoeren ihn
+
+				dnDes.parentNode.removeChild( dnDes );
+
+			}
+
+		});
+
 		}
+
 	}
 	/*
 	* adds a span with text for the right DOM element
@@ -87,24 +107,24 @@ FormHelper = {
 		switch(nameOfElement){
 
 			case "sTitle":
-				span.innerHTML = "Title of the book you are reviewing";
-				break;
+			span.innerHTML = "Title of the book you are reviewing";
+			break;
 
 			case "sTagline":
-				span.innerHTML = "A one-sentence tagline";
-				break;	
+			span.innerHTML = "A one-sentence tagline";
+			break;	
 
 			case "sShort":
-				span.innerHTML = "A short summary of your review";
-				break;
+			span.innerHTML = "A short summary of your review";
+			break;
 
 			case "sLong":
-				span.innerHTML = "Here you enter the actual review";
-				break;
+			span.innerHTML = "Here you enter the actual review";
+			break;
 
 			default:
-				span.innerHTML = "something went terribly wrong!";
-				break;						
+			span.innerHTML = "something went terribly wrong!";
+			break;						
 		}
 		console.log(span);
 		return span;
